2ebb329fa71299d92a92f73bc4d71136b8ed5b75
Steigher Devia Zapata <steigher@gmail.com>
2025-06-18 22:41:19 -0500

    poo nueva tema poo inicializacion

--- comandos.js
+++ comandos.js
@@ -3,4 +3,13 @@ echo "# Fundamentos" >> README.md = crea nuestro redmi
 git add . : el . significa que vamos a subir todos los nuevos cambios (las carpetas que estàn en verde)
 si se reemplaza el . por la carpeta especifica, solo se sube la carpeta
 
+Antes de hacer el commit hay que agregar el git add 
+
+Git branch muestra las ramas que hay
+
+git remote add origin https://github.com/Steigher/Fundamentos.git
+sirve para enlazar nuestro proyecto de vsc al github
+
+git push origin main
+para subir el codigo a GIT
 */
