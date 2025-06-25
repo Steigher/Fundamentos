f474190804d60c58981910e3691f9070a4b4878d
Steigher Devia Zapata <steigher@gmail.com>
2025-06-24 22:14:15 -0500

    ramapullrequest

--- github/ramapullrequest.js
+++ github/ramapullrequest.js
@@ -4,4 +4,6 @@ PAra ver las ramas = git branch
 Para crear una nueva rama = git checkout -b "nombre de la rama". (ej: git checkout -b poo )
 para cambiar de rama = git checkout "nombre de la rama" (ej: git checkout main )
 para mezclar las rama, 1. ubicarse en la carpeta principal, 2. git merge "nombre de la rama" (git merge poo)
+
+git status = para revisar que no se ha cargado
 */
